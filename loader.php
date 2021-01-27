067fd52f152ff80a436b6142a6268571265a4c42
awnishmmg <55636215+awnishmmg@users.noreply.github.com>
2021-01-27 21:55:44 +0530

    Updated Loader Class

--- loader.php
+++ loader.php
@@ -2,24 +2,101 @@
 
 class Load{
 
+	//Model Folder
 	public static function model($file){
 
-		include dirname(__DIR__)."/".__FUNCTION__."/{$file}_model.php";
+		$path=dirname(__DIR__)."/".__FUNCTION__."/{$file}_model.php";
+		if(file_exists($path)):
+			$value=basename($path,'_model.php');
+			include $path;
+		else:	
+			die('<b> Such File does not exist </b>');
+		endif;
 	}
 
+	// Helper Folder
 	public static function helper($file){
 		
-		include dirname(__DIR__)."/".__FUNCTION__."/{$file}_helper.php";
+		$path=include dirname(__DIR__)."/".__FUNCTION__."/{$file}_helper.php";
+		if(file_exists($path)):
+			$value=basename($path,'_helper.php');
+			include $path;
+		else:
+			die('<b> Such File does not exist </b>');
+		endif;
 	}
 
+	// Library Folder
 	public static function library($file){
 		
-		include dirname(__DIR__)."/".__FUNCTION__."/{$file}.class.php";
+		$path=dirname(__DIR__)."/".__FUNCTION__."/{$file}.class.php";
+		if(file_exists($path)):
+				$value=basename($path,'.class.php');
+				include $path;
+		else:
+				die('<b> Such File does not exist </b>');	
+		endif;
 	}
 
+	//Config Folder
 	public static function config($file){
 		
-		include dirname(__DIR__)."/".__FUNCTION__."/{$file}.php";
+		$path=dirname(__DIR__)."/".__FUNCTION__."/{$file}_config.php";
+		if(file_exists($path)):
+				$value=basename($path,'_config.php');
+				include $path;
+		else:
+				die('<b> Such File does not exist </b>');
+		endif;
+	}
+
+	//package Folder
+	public static function package($file){
+		
+		$path=dirname(__DIR__)."/".__FUNCTION__."/{$file}_package.php";
+		if(file_exists($path)):
+				$value=basename($path,'_package.php');
+				include $path;
+		else:
+				die('<b> Such File does not exist </b>');
+		endif;
+	}
+
+	//Any General PHP File
+	public static function php($file,$from){
+		
+		$path=dirname(__DIR__)."/".$from."/{$file}.php";
+		if(file_exists($path)):
+				$value=basename($path,'.php');
+				include $path;
+		else:
+				die('<b> Such File does not exist </b>');
+		endif;
+	}
+
+
+	//Any General PHP File with Folder Name
+	public static function php_file($file){
+		
+		$path=dirname(__DIR__)."/".$from."/{$file}.php";
+		if(file_exists($path)):
+				$value=basename($path,'.php');
+				include $path;
+		else:
+				die('<b> Such File does not exist </b>');
+		endif;
+	}
+
+	//Any File with Root Folder and File name with Any Extension
+	public static function file($file){
+		
+		$path=dirname(__DIR__)."/".$from."/{$file}";
+		if(file_exists($path)):
+				$value=basename($path);
+				include $path;
+		else:
+				die('<b> Such File does not exist </b>');
+		endif;
 	}
 
 
