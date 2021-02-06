870dab7a9df3b421c6a072faf326c36e2b32a0ed
awnishmmg <55636215+awnishmmg@users.noreply.github.com>
2021-02-06 17:34:14 +0530

    changes done

--- hooks/binding.php
+++ hooks/binding.php
@@ -2,8 +2,7 @@
 	
 	$json_str=file_get_contents(dirname(__DIR__).'/view/template.json');
 	$json_arr=json_decode($json_str,true);
-	$page_key= basename($_SERVER['PHP_SELF'],'.php');
-	
+	$page_key= basename($_SERVER['PHP_SELF'],'.php');	
 	if(array_key_exists($page_key, $json_arr)){
 		@$json_data_1=$json_arr[$page_key];
 		foreach (@$json_data_1 as $key => $value) {
@@ -11,6 +10,5 @@
 		
 		}
 	}
-
 
 ?>
--- hooks/hooks.php
+++ hooks/hooks.php
@@ -1,15 +1,23 @@
 <?php
 	
 return [
-
+	
 		'prepend_keys'=>['header','config','binding'],
 		'append_keys'=>['footer'],
-		//define Keys
+		'disallow_keys'=>[
+		'ajax'=>
+			[
+				'template_header',
+				'binding',
+				'template_footer',
+			],
+		],
+
 		'config'=>dirname(__DIR__).'/config/autoloader.php',
 		'binding'=>dirname(__DIR__).'/hooks/binding.php',
-		'header'=>dirname(__DIR__).'/view/header.php',
+		'header'=>dirname(__DIR__).'/view/template_header.php',
 		'session'=>'',
-		'footer'=>dirname(__DIR__).'/view/footer.php',
+		'footer'=>dirname(__DIR__).'/view/template_footer.php',
 
 ];
 
