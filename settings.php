b235f40f44704b024fefb7e5f8922c2257047540
awnishmmg <55636215+awnishmmg@users.noreply.github.com>
2021-02-06 17:08:04 +0530

    New  updates settings

--- settings.php
+++ settings.php
@@ -1,18 +1,24 @@
 <?php
-
+define('DOCUMENT_ROOT',dirname(__DIR__));
 //Save All the Setting
 return [
 	
 		'constants'=>[
 
-			'SYSTEM_PATH'=>'system',
+			//Note System Path is for File System or File Handling Stuff
+			'SYSTEM_PATH'=>DOCUMENT_ROOT,
 
-			'APP_PATH'=>'http://localhost/myproject/example/web-app/',
+			'APP_PATH'=>'http://localhost/myproject/way2mint/compaint-Application/web-app/',
 
 			'API_PATH'=>'',
 
-			'ADMIN_PATH'=>'http://localhost/myproject/example/web-app/admin/',
-			'AJAX_PATH'=>'http://localhost/myproject/example/web-app/ajax/',
+			'ADMIN_PATH'=>'http://localhost/myproject/way2mint/compaint-Application/web-app/admin/',
+			'AJAX_PATH'=>'http://localhost/myproject/way2mint/compaint-Application/web-app/ajax/',
+
+			//File Uploading Setting
+			'FILE_UPLOAD_PATH'=>dirname(__DIR__).'/uploads/',
+			'FILE_MAX_SIZE' =>'5',
+
 		],
 
 		'database'=>[
@@ -44,4 +50,10 @@ return [
 
 	'app_key'=>'secret_key',
 	'app_password'=>'1234', 
+
+	'pass_encrypt' => 'md5',
+	'salt_hash' => '!!test^&*#2002@',
 ];
+
+
+
